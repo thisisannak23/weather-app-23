2804ad5f136823a3b2c9685ed157eb81baaeb39f
thisisannak23 <anna.kazanecki@gmail.com>
2023-11-10 14:57:56 -0600

    Added links to GitHub and Portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="" target="_blank">
+          Anna
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/thisisannak23/weather-app-23"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
