5a18fc3feed0ff9b891fa510492ff096248aaa68
thisisannak23 <anna.kazanecki@gmail.com>
2023-11-10 15:18:59 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container"></div>
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
